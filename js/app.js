9a9f78e286d836c539687c54672a075b4a8625e7
Morella92 <morena.piem@outlook.com>
2023-02-13 18:09:45 +0100

    START js simon exercise

---
+++ js/app.js
@@ -0,0 +1,47 @@
+console.log('Js Simon')
+
+// Imposto un timer di 30 secondi.
+setTimeout (myFunction, 30000)
+
+// Visualizzare in pagina 5 numeri casuali con alert.
+const numbers = []
+
+while(numbers.length < 5 ){
+    let randomNumbers= Math.floor(Math.random() * 5) + 1
+    console.log(randomNumbers)
+
+    numbers.push(randomNumbers)  
+}
+
+alert(numbers.join('' , ''))
+
+// Allo scadere dei 30 secondi l’utente deve inserire, uno alla volta,
+// i numeri che ha visto precedentemente, tramite il prompt(). 
+function myFunction (){
+   
+    let userNumbers = []
+
+    while(userNumbers.length < 5){
+
+        let askUser = parseInt(prompt('Inserisci i numeri giusti per vincere!'))
+        console.log(askUser)
+
+        userNumbers.push(askUser)
+    }  
+}
+
+// Dopo che sono stati inseriti i 5 numeri, 
+// il software dice quanti e quali numeri sono stati individuati tramite console.log
+
+let rightNumbers = 0
+
+for(let i = 0; i < 5; i++){
+
+    if(userNumbers[i] === numbers[i]){
+
+        rightNumbers++
+
+    }
+}
+
+console.log('Hai indovinato' + rightNumbers + 'numeri!')
