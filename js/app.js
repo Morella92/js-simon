1aa214e8869011e5b2ab47574a358d71ca6463c3
Morella92 <morena.piem@outlook.com>
2023-02-14 10:13:03 +0100

    FEAT function + correction

--- js/app.js
+++ js/app.js
@@ -6,14 +6,7 @@ setTimeout(myFunction, 30000)
 // Visualizzare in pagina 5 numeri casuali con alert.
 let numbers = []
 
-while (numbers.length < 5) {
-    let randomNumbers = Math.floor(Math.random() * 5) + 1
-    console.log(randomNumbers)
-
-    numbers.push(randomNumbers)
-}
-
-alert(numbers.join('', ''))
+generateRandomNumbers()
 
 // Allo scadere dei 30 secondi l’utente deve inserire, uno alla volta,
 // i numeri che ha visto precedentemente, tramite il prompt(). 
@@ -26,12 +19,13 @@ function myFunction() {
         let askUser = parseInt(prompt('Inserisci i numeri giusti per vincere!'))
         console.log(askUser)
 
-        userNumbers.push(askUser)
+        if (numbers.includes(askUser)) {
 
-        if (userNumbers === isNaN || userNumbers === '') {
+            console.log('Indovinato')
 
-            alert('Devi inserire un numero valido per continuare!')
         }
+
+        userNumbers.push(askUser)
     }
 
     // Dopo che sono stati inseriti i 5 numeri, 
@@ -39,6 +33,24 @@ function myFunction() {
 
     let rightNumbers = []
     
+    compareNumbers(rightNumbers)
+
+    console.log('Hai indovinato '+ rightNumbers.length + ' numeri ' + rightNumbers)
+
+}
+
+function generateRandomNumbers() {
+    while (numbers.length < 5) {
+        let randomNumbers = Math.floor(Math.random()* 5) + 1
+        console.log(randomNumbers)
+
+        numbers.push(randomNumbers)
+    }
+
+    alert(numbers.join('', ''))
+}
+
+function compareNumbers(rightNumbers) {
     for (let i = 0; i < 5; i++) {
 
         if (numbers.includes(userNumbers[i])) {
@@ -48,8 +60,5 @@ function myFunction() {
         }
 
     }
-
-    console.log('Hai indovinato '+ rightNumbers.length + ' numeri ' + rightNumbers)
-
 }
 
